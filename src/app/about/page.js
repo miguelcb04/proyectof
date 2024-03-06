1ff8d789e04df543e0fe14d11183ab6d7b57b2b5
Miguel Chacón Barranco <miguelcb127@gmail.com>
2024-03-06 16:32:08 +0100

    cuarto

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,11 +2,14 @@
 
 function About() {
   return (
-    <>
-<h1 class="text-3xl font-bold mb-4">Bike Tech</h1>
-    <p class="text-lg">Bike Tech es una tienda especializada en bicicletas que ofrece una amplia gama de productos y servicios relacionados con la tecnología aplicada al mundo del ciclismo. Desde la venta de bicicletas de última generación hasta accesorios innovadores y servicios de mantenimiento avanzado, Bike Tech se destaca por su compromiso con la excelencia técnica y la satisfacción del cliente en el ámbito de la movilidad ciclista.</p>
-    </>
-  )
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f3f4f6' }}>
+      <div className="card" style={{ backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+        <h1 className="text-3xl font-bold mb-4">Bike Tech</h1>
+        <p className="text-lg">Bike Tech es una tienda especializada en bicicletas que ofrece una amplia gama de productos y servicios relacionados con la tecnología aplicada al mundo del ciclismo. Desde la venta de bicicletas de última generación hasta accesorios innovadores y servicios de mantenimiento avanzado, Bike Tech se destaca por su compromiso con la excelencia técnica y la satisfacción del cliente en el ámbito de la movilidad ciclista.</p>
+      </div>
+    </div>
+  );
+  
 }
 
 export default About
